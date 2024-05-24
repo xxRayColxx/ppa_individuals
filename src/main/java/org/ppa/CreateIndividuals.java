772c058e1901ed0007e91d42ae9a82f66c1a9606
Elena <elena.slavco@triply.cc>
2024-05-24 13:30:11 +0200

    Added foaf prefix

--- src/main/java/org/ppa/CreateIndividuals.java
+++ src/main/java/org/ppa/CreateIndividuals.java
@@ -76,7 +76,9 @@ public class CreateIndividuals {
       "prefix owl: <http://www.w3.org/2002/07/owl#>\n" +
       "prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
       "prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
-      "prefix xsd: <http://www.w3.org/2001/XMLSchema#>\n";
+      "prefix xsd: <http://www.w3.org/2001/XMLSchema#>\n" + 
+      "prefix foaf: <http://xmlns.com/foaf/0.1/>\n";
+
 
    /**
     * Constructor of the program
